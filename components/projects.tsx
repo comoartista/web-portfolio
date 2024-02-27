08ef64bb33fba98c1aed6ee32d999e2cdf47fcf2
Oksana Melnyk <oks.melnuk@gmail.com>
2024-02-27 15:01:50 +0100

    SectionHeadings

---
+++ components/projects.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SectionHeadings from "./section-heading";
+export default function Projects() {
+  return (
+    <section>
+      <SectionHeadings>Projects</SectionHeadings>
+      <div></div>
+    </section>
+  );
+}
